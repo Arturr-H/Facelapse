1de64833e8dfcdfe8d7cbac84b02302ae6a0a1a0
Artur <artur.h.0427@gmail.com>
2023-06-04 15:57:48 +0200

    Include more screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { TransitionPresets, createStackNavigator } from "@react-navigation/stack";
 import MainScreen from "./Main";
 import Login from "./scenes/account/Login";
 import Register from "./scenes/account/Register";
 import SignUp from "./scenes/account/SignUp";
 import StackScene from "./scenes/stack/Stack";
 import Calibration from "./scenes/calibration/Main";
+import StackScene2 from "./scenes/stack/StackSecond";
 
 const Stack = createStackNavigator();
 
@@ -14,9 +15,10 @@ const App = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen options={{ headerShown: false }} name="Calibration" component={Calibration} />
 				<Stack.Screen options={{ headerShown: false }} name="Home" component={MainScreen} />
+				<Stack.Screen options={{ headerShown: false }} name="StackScene2" component={StackScene2} />
 				<Stack.Screen options={{ headerShown: false }} name="Stack" component={StackScene} />
+				<Stack.Screen options={{ headerShown: false }} name="Calibration" component={Calibration} />
 				<Stack.Screen options={{ headerShown: false }} name="SignUp" component={SignUp} />
 				<Stack.Screen options={{ headerShown: false }} name="Register" component={Register} />
 				<Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
